72eb984e56d68f7c1b5b115a2ae4d2df4a4d2fd0
Nick Molargik <nick@molargiksoftware.com>
2025-07-11 11:31:32 -0400

    fix some icons

--- src/pages/Sweetwater.tsx
+++ src/pages/Sweetwater.tsx
@@ -1,15 +1,27 @@
+import comingsoonIcon from '../assets/comingsoonicon.svg';
 import sweetwatericon from '../assets/sweetwater/sweetwatericon.svg';
 import Hero from '../components/Hero';
 
 export default function Sweetwater() {
   return (
-    <Hero
-      heading="Previous Employment - Sweetwater"
-      description="Application development with a focus on user-facing experiences and intuitive design."
-      imageSrc={sweetwatericon}
-      buttonText="View on the App Store"
-      buttonColorClass="bg-orange-500 text-white"
-      buttonHref="https://apps.apple.com/us/app/sweetwater-music-audio-gear/id1448496608"
-    />
+    <>
+      <Hero
+        heading="Previous Employment - Sweetwater"
+        description="The Sweetwater Sound app is a mobile storefront that lets musicians browse, demo-listen to, and purchase Sweetwater’s full catalog of instruments and pro-audio gear with personalized gear advice, order tracking, and tech support built in."
+        imageSrc={sweetwatericon}
+        buttonText="View on the App Store"
+        buttonColorClass="bg-orange-500 text-white"
+        buttonHref="https://apps.apple.com/us/app/sweetwater-music-audio-gear/id1448496608"
+      />
+
+      {/* Coming soon badge */}
+      <div className="mt-8 flex justify-center">
+        <img
+          src={comingsoonIcon}
+          alt="Coming Soon"
+          className="w-32 md:w-40"
+        />
+      </div>
+    </>
   );
 }
--- src/pages/V1Sports.tsx
+++ src/pages/V1Sports.tsx
@@ -1,15 +1,33 @@
 import v1icon from '../assets/v1sports/v1icon.svg';
+import comingsoonIcon from '../assets/comingsoonicon.svg';
 import Hero from '../components/Hero';
 
 export default function V1Sports() {
   return (
+    <>
     <Hero
       heading="Employment - V1 Sports"
-      description="Analyze your swing and improve with video & data."
+      description="Pair with a coach or athlete, analyze your swing, and improve with video & data."
       imageSrc={v1icon}
       buttonText="View on the App Store"
       buttonColorClass="bg-orange-500 text-white"
       buttonHref="https://apps.apple.com/us/app/v1-coach-video-analysis-app/id6476321492"
     />
+
+      {/* Coming soon badge */}
+      <div className="mt-8 flex justify-center">
+        <img
+          src={comingsoonIcon}
+          alt="Coming Soon"
+          className="w-32 md:w-40"
+        />
+      </div>
+    </>
+
+
+
   );
+
+
+
 }
